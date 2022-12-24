ed5992458b6c51e64b0e2e31c29d678dfd42d16f
AnastasiiaPiatak <iamsherlocked031199@gmail.com>
2022-12-24 15:40:43 +0200

    Added a dark mode colors
    Changed dark mode icons on images

--- src/script.js
+++ src/script.js
@@ -1,9 +1,27 @@
-let btn = document.getElementById("toggleBtn");
-let body = document.querySelector("body");
+let toggleBtn = document.getElementById("toggleBtn");
+toggleBtn.addEventListener("click", changeTheme);
+let toggleImg = document.getElementById("toggleImg");
 
-console.log(btn);
+function changeTheme() {
+  if (document.body.classList.contains("dark")) {
+    setLightTheme();
+  } else {
+    setDarkTheme();
+  }
+}
 
-btn.onclick = function () {
-  this.classList.toggle("dark");
-  body.classList.toggle("dark");
-};
+function setDarkTheme() {
+  document.body.classList.add("dark");
+  toggleImg.src = "img/sun.png";
+  localStorage.theme = "dark";
+}
+
+function setLightTheme() {
+  document.body.classList.remove("dark");
+  toggleImg.src = "img/moon.png";
+  localStorage.theme = "light";
+}
+
+if (localStorage.theme === "dark") {
+  setDarkTheme();
+}
